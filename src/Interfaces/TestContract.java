88dcd9298921c4534a0e5a7eb9da0bad48f264fb
Rodrigo Sampaio <carneirosampaior@gmail.com>
2021-09-08 00:07:08 -0300

    commit aula 8 atualização log(),linha().

---
+++ src/Interfaces/TestContract.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+public abstract class TestContract {
+   public static void log(String message) {
+        System.out.println(message);
+    }
+
+    public static void linha(){
+        System.out.println("-----------------------------------------");
+    }
+}
--- src/poo_aula_8/Teste.java
+++ src/poo_aula_8/Teste.java
@@ -1,16 +1,22 @@
 package poo_aula_8;
 
-public class Teste {
+import Interfaces.TestContract;
+
+public class Teste extends TestContract {
     public static void main(String[] args) {
         Personagem mario = new Personagem("Mario",1.50,52);
         Personagem luigi = new Personagem("Luigi",1.75,32);
 
         Jogo jogo = new Jogo(mario,luigi);
+        log("------------------SUPER MARIO GAME----------------");
+        linha();
         jogo.statusDoJogador();
+        linha();
+        log("---------------------GAME START-------------------");
         jogo.playGame();
+        linha();
+        log("----------------------END GAME--------------------");
         jogo.statusDoJogador();
-
 
     }
-
 }
